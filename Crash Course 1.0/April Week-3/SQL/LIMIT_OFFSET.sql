6dc7337f77ced01122456ac1bc6ed3261344c699
rishabh2298 <rishabhmishra2298@gmail.com>
2024-04-21 00:27:09 +0530

    Avg_salary (mathematical calculation), LIMIT 5 OFFSET 0 - Top 5 highest paying position

--- Crash Course 1.0/April Week-3/SQL/LIMIT_OFFSET.sql
+++ Crash Course 1.0/April Week-3/SQL/LIMIT_OFFSET.sql
@@ -152,3 +152,22 @@ ORDER BY
 	max_salary DESC
 LIMIT
 	5 OFFSET 0;
+
+
+
+-- Problem 3 : Create a query that displays the top five highest paying positions. 
+-- Take the mean value of the columns into account:
+-- * min_salary
+-- * max_salary
+-- To do this, create a new column called avg_salary that will contain the average 
+-- value of the above-mentioned columns. In the result table, display all columns 
+-- of the job table including the avg_salary column (as the last column).
+
+SELECT
+	*, (max_salary + min_salary)/2 AS avg_salary
+FROM
+	job
+ORDER BY
+	max_salary DESC
+LIMIT
+	5 OFFSET 0;
