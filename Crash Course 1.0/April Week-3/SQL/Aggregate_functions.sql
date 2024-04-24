0639849eace9564057797ee44efd7b544361462f
rishabh2298 <rishabhmishra2298@gmail.com>
2024-04-24 18:00:27 +0530

    NotNULL, COUNT()

--- Crash Course 1.0/April Week-3/SQL/Aggregate_functions.sql
+++ Crash Course 1.0/April Week-3/SQL/Aggregate_functions.sql
@@ -1,4 +1,5 @@
 
+-- Link : https://www.notion.so/Aggregate-Functions-c97f61060aaf4f929a9e840e2e21bf72
 
 -- Problem 1 : Create a query that displays the number of records in the Stock table.
 
@@ -7,3 +8,13 @@ SELECT
 FROM
  	Stock;
 
+
+-- Problem 2 :Create a query that displays the number of non-empty values in the EPS column. 
+-- Assign the alias NonMissingEPS to the result.
+
+SELECT
+	COUNT(EPS) as NonMissingEPS
+FROM
+	Stock
+WHERE
+	EPS NotNULL;
