1e5bee5ac8e869b4648f6089851489cb8c868c16
rishabh2298 <rishabhmishra2298@gmail.com>
2024-04-25 23:40:32 +0530

    GROUP BY, ORDER BY

--- Crash Course 1.0/April Week-3/SQL/GroupBy_part1.sql
+++ Crash Course 1.0/April Week-3/SQL/GroupBy_part1.sql
@@ -11,3 +11,18 @@ FROM
 	Stock
 GROUP BY
 	sector;
+
+
+
+-- Problem 2 : Group data at the Sector level. Then, calculate the number of companies for 
+-- each sector and assign an alias NumOfStocks. Sort the result by the NumOfStocks 
+-- column in descending order.
+
+SELECT
+	sector, COUNT(*) AS NumOfStocks
+FROM
+	Stock
+GROUP BY
+	sector
+ORDER BY
+	NumOfStocks DESC;
